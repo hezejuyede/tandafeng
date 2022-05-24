a1eeed52e2520f9c45724e4ee5457e8b41d32024
qieerxi <1574408801@qq.com>
2022-05-24 14:09:25 +0800

    14：09分

--- src/common/shadinLayer.vue
+++ src/common/shadinLayer.vue
@@ -5,6 +5,7 @@
         <div
             style="font-size:50px; text-align:center; position: fixed; top: 0; left: 0; right: 0; bottom: 0; z-index: 99999;">
             <i class="el-icon-loading" style="margin-top:20%;font-size: 200%;color: #409EFF"></i>
+            <span style="font-size: 18px;color: #2d8cf0;display: block;margin-top: 10px">数据载入中......</span>
         </div>
     </div>
 </template>
